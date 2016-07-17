44268e697b53c4287ee8b533a9653c7a9072e46b
Ryan <rymbox21@yahoo.com>
2016-07-17 16:07:15 +0800

    Add files via upload

---
+++ LED_run.ino
@@ -0,0 +1,17 @@
+
+void setup() {
+  pinMode (A0, OUTPUT);
+  pinMode (A1, OUTPUT);
+  pinMode (A2, OUTPUT);
+ }
+
+void loop () {
+  int ledPin[] = {A0, A1, A2};
+  int i;
+  for (i=0; i < 3; i++) {
+     digitalWrite(ledPin[i], HIGH);
+     delay (30);
+     digitalWrite(ledPin[i], LOW);
+     delay (30);
+    }
+}
